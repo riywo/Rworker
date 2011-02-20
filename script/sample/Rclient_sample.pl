506afea76f5014a1bb51f23d983016255d42b9a9
riywo <riywo.jp@gmail.com>
2011-02-21 01:26:11 +0900

    modify misc

--- script/sample/Rclient_sample.pl
+++ script/sample/Rclient_sample.pl
@@ -15,7 +15,7 @@ my $arg1 = encode_json({
     img  => "$dir/img/sample_local.png",
 });
 my $arg2 = encode_json({
-    r_file    => "https://gist.github.com/raw/829619/3cf65e97afe00c7fc0a1ebb1a9fb48c7c9ae3653/sample.r",
+    r_file    => "https://gist.github.com/raw/829619/0dfe0de486d0044627396bc43b3c8bf47f12a962/sample.r",
     r_return  => "http://localhost:5000/api/job/1/log?log=",
 #    r_file    => "$dir/r_file/sample.r",
 #    r_return  => "$dir/r_return/test.log",
@@ -23,7 +23,7 @@ my $arg2 = encode_json({
     data => "https://gist.github.com/raw/829631/ed027079cc5c1557fd4a6fefd4afc81e31cc384a/cars.csv",
     img  => "/tmp/sample.png",
 
-    upload_uri => "http://localhost:5000/api/job/1/upload",
+    upload_url => "http://localhost:5000/api/job/1/upload",
     upload_key => "img",
 });
 
@@ -36,6 +36,6 @@ $client->job_servers('127.0.0.1');
 $client->do_task('Rworker', $arg1);
 $client->do_task('Rworker', $arg2);
 
-#$client->dispatch_background('Rworker', $args1);
-#$client->dispatch_background('Rworker', $args2);
+#$client->dispatch_background('Rworker', $arg1);
+#$client->dispatch_background('Rworker', $arg2);
 
